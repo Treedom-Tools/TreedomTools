f82f9980a50d876de6563aa0a51e56ced6969a8e
teamtreedom <43453493+teamtreedom@users.noreply.github.com>
2018-10-10 11:18:28 -0400

    Updating org code with latest

--- TreedomTools/Python/Test-Firebase.py
+++ TreedomTools/Python/Test-Firebase.py
@@ -5,10 +5,10 @@ from Firebase import db_functions
 FIREBASE_KEY_PATH = "C:/Users/Treedom/Desktop/treedom-tools-firebase-adminsdk-xn36r-5f0eda3b35.json"
 db = firestore.Client.from_service_account_json(FIREBASE_KEY_PATH)
 
-# response_dict = db_functions.add_document('volunteers',Volunteer("Test user flow to phone",26).to_dict())
-# print(response_dict)
+response_dict = db_functions.add_document('volunteers',Volunteer("Michael Kadisha",24).to_dict())
+print(response_dict)
 
-db_functions.delete_document('volunteers/soespaBUm8FTn1NKsiSL')
+# db_functions.delete_document('volunteers/7HDLfkdpzuGn4cgIU6nJ')
 # print(response_dict)
 
 # new_volunteer_ref = db.collection('volunteers').document(None)
